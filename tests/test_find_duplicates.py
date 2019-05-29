5af07101e3b78628cb8104e204ddc1b78cbae22a
Justin Shenk <shenkjustin@gmail.com>
2019-05-29 08:55:09 +0200

    Update test

--- tests/test_find_duplicates.py
+++ tests/test_find_duplicates.py
@@ -13,12 +13,11 @@ from simages import find_duplicates, Embeddings
 def test_find_duplicates(hw):
     data = np.random.random((100, hw, hw))
     data = data[:, np.newaxis, ...]
-    embeddings = Embeddings(array=data, show_train=False)
+    embeddings = Embeddings(data, show_train=False)
 
     n = 3
     pairs, distances = embeddings.duplicates(n=n)
     assert len(pairs) >= n
 
-    # Prevent argparsing of pytest args
-    pairs, distances = find_duplicates(data, num_channels=1, show_train=False)
+    pairs, distances = find_duplicates(array=data, num_channels=1, show_train=False)
     assert len(pairs) >= n
