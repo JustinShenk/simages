3c8bbf061ad6e4d5b7a5df52bfb8f45e2847af03
Justin Shenk <shenkjustin@gmail.com>
2019-05-30 18:24:24 +0200

    Add docs

---
+++ docs/examples/plot_comparing.py
@@ -0,0 +1,10 @@
+"""
+Comparing
+---------
+simages allows comparing trajectories using various methods.
+"""
+import simages
+import numpy as np
+
+X = np.random.random((100,28,28))
+simages.find_duplicates(X, num_channels=1, show=True)
