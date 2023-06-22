a51388895fafeedcd9be7a3ffdceb3ff886f2e40
Justin Shenk <shenk.justin@gmail.com>
2023-06-22 14:09:55 +0200

    Bump version

--- src/simages/__init__.py
+++ src/simages/__init__.py
@@ -6,7 +6,7 @@ from .embeddings import Embeddings, linkageplot
 from .extractor import EmbeddingExtractor
 from .main import find_duplicates
 
-__version__ = "23.0.2"
+__version__ = "23.0.3"
 
 __title__ = "simages"
 __description__ = "Find similar images in a dataset"
